af82c311ff918e098dfd4c93dad998dfa577afc5
ncioo <60670380+ncioo@users.noreply.github.com>
2024-03-17 23:28:10 +0300

    Create .prettierrc.js

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+	trailingComma: 'none',
+	printWidth: 100,
+	useTabs: true,
+	tabWidth: 4,
+	singleQuote: true,
+	endOfLine: 'lf',
+	arrowParens: 'avoid',
+	bracketSpacing: true
+};
